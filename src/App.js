349443cc878c46911235d67bb07215d43a555c13
SANJAIDEVARAJ <sanjaidevaraj@gmail.com>
2020-08-18 10:01:56 +0530

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import FileUpload from './components/FileUpload';
+
+
+const App = () => (
+  <div className='container mt-4'>
+    <h4 className='display-4 text-center mb-4'>
+     GOOGLE DRIVE
+    </h4>
+
+    <FileUpload />
+  </div>
+);
+
+export default App;
